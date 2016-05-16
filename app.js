5e1cb77e4600ea8f17b2ab9ac6567f820ee066ef
Dragos Rosu <r.dragosh@yahoo.com>
2016-05-16 22:38:18 +0100

    added sticky footer

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.static(__dirname + '/public'));
 //app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 //app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/flexboxgrid/css')); // redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/styles', express.static(__dirname + '/styles'));
 app.set('port', (process.env.PORT || 5000));
 
