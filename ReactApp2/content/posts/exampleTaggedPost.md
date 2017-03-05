7efc4ea23855311316eb0edf8fa3bee727641301
rosudrag <r.dragosh@yahoo.com>
2017-03-05 22:23:30 +0000

    add basic tagging

---
+++ ReactApp2/content/posts/exampleTaggedPost.md
@@ -0,0 +1,10 @@
+---
+title: Example Tagged Post
+date: 2017-03-03
+layout: Post
+tags: music
+---
+
+This is the first post!
+
+Kasia is the best
--- ReactApp2/src/layouts/Post/index.js
+++ ReactApp2/src/layouts/Post/index.js
@@ -5,9 +5,12 @@ import Page from "../Page"
 
 import styles from "./index.css"
 
+const defaultTags = "general";
+
 const Post = (props) => {
   // it's up to you to choose what to do with this layout ;)
-  const pageDate = props.head.date ? new Date(props.head.date) : null
+  const pageDate = props.head.date ? new Date(props.head.date) : null;
+  const tags = props.head.tags ? props.head.tags : defaultTags;
 
   return (
     <Page
@@ -15,12 +18,15 @@ const Post = (props) => {
       header={
         <div>
           <header className={ styles.header }>
-            {
-              pageDate &&
-              <time key={ pageDate.toISOString() }>
-                { pageDate.toDateString() }
-              </time>
-            }
+              {
+                pageDate &&
+                <time key={ pageDate.toISOString() }>
+                  { pageDate.toDateString() }
+                </time>
+              }
+              <span>
+                 Tags: { tags }
+              </span>
           </header>
         </div>
       }
