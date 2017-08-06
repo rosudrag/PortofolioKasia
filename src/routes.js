1cc6199b46223460cea2cc9e3adacd03f6f1cd70
rosudrag <r.dragosh@yahoo.com>
2017-08-07 00:02:56 +0100

    feat: add GA

--- src/routes.js
+++ src/routes.js
@@ -8,21 +8,30 @@ import PageError from './layouts/PageError';
 import Homepage from './layouts/Homepage';
 import TaggedBlogPage from './layouts/TaggedBlogPage';
 import Post from './layouts/Post';
+import Scripts from './scripts';
+
+var ReactGA = require('react-ga');
+ReactGA.initialize('UA-77562801-1');
+
+function logPageView() {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 const PageContainer = (props) => (
   <div>
       <PhenomicPageContainer
-      { ...props }
-      layouts={{
-          Page,
-          PageError,
-          Homepage,
-          Post,
-          TaggedBlogPage
-      }}
-    />
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
+        { ...props }
+        layouts={{
+            Page,
+            PageError,
+            Homepage,
+            Post,
+            TaggedBlogPage
+        }}
+      />
+      <Scripts/>
+
   </div>
 
 );
@@ -30,7 +39,7 @@ const PageContainer = (props) => (
 export default (
   <div>
     <Route component={ AppContainer }>
-      <Route path="*" component={ PageContainer } />
+      <Route path="*" component={ PageContainer } onEnter={logPageView}/>
     </Route>
   </div>
 );
---
+++ src/scripts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scripts = () => {
+
+    return (
+        <div style={{ display: 'none' }}>
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css"/>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
+        </div>
+    );
+};
+
+export default Scripts;
