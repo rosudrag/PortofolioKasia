8300ed4b0f73d3135061eedb91631ddaf8181851
rosudrag <r.dragosh@yahoo.com>
2017-09-14 00:49:55 +0100

    feat: wip layout

--- src/layouts/Page/index.js
+++ src/layouts/Page/index.js
@@ -1,71 +1,56 @@
 import Head from 'react-helmet';
 import React from 'react';
-import PropTypes from 'prop-types';
+import { createMuiTheme } from 'material-ui/styles';
+import NavBar from '../../NavBar';
+import { MuiThemeProvider } from 'material-ui/styles';
+import { red, blue, teal } from 'material-ui/colors';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
 import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
 import './styles.css';
 
-
-const styles = theme => ({
-    root: {
-        marginTop: theme.spacing.unit * 0,
-        width: '100%'
-    },
-    flex: {
-        flex: 1
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20
+const theme = createMuiTheme({
+    palette: {
+        primary: blue, // Purple and green play nicely together.
+        secondary: teal,
+        error: red
     }
 });
 
-function ButtonAppBar(props) {
-    const classes = props.classes;
-    return (
+const styles = aTheme => ({
+    root: {
+        flexGrow: 1
+    },
+    paper: {
+        padding: 16,
+        textAlign: 'center',
+        color: aTheme.palette.text.secondary
+    }
+});
+
+
+const Layout = ({ children, classes }) => (
+  <MuiThemeProvider theme={theme}>
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>
-            Portfolio
-          </Typography>
-          <Button color="contrast"><a href='/music'>Test</a></Button>
-        </Toolbar>
-      </AppBar>
+      <Head>
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+      </Head>
+      <NavBar/>
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <div className={classes.paper}>{children}</div>
+          </Grid>
+        </Grid>
+      </div>
     </div>
-    );
-}
-
-ButtonAppBar.propTypes = {
-    classes: PropTypes.object.isRequired
-};
-
-const TryMe = withStyles(styles)(ButtonAppBar);
-
-const Layout = ({ children }) => (
-  <div>
-    <Head>
-      <html lang="en" />
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css"/>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-    </Head>
-  <div>
-    <TryMe/>
-		{children}
-	</div>
-  </div>
+  </MuiThemeProvider>
 );
 
-export default Layout;
+export default withStyles(styles)(Layout);
