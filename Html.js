27568b1550f698b56c883901627fecbd9fb8f51e
rosudrag <r.dragosh@yahoo.com>
2017-09-20 21:22:33 +0100

    feat: default phenomic template for debug

--- Html.js
+++ Html.js
@@ -7,7 +7,7 @@ export default ({ App, render }: PhenomicHtmlPropsType) => {
   const { Main, State, Script, Style } = render(<App />);
   const helmet = Head.renderStatic();
   return (
-    <html {...helmet.htmlAttributes.toComponent()} className="no-js" class="no-js" lang="">
+    <html {...helmet.htmlAttributes.toComponent()}>
       <head>
         {helmet.meta.toComponent()}
         {helmet.title.toComponent()}
@@ -17,31 +17,11 @@ export default ({ App, render }: PhenomicHtmlPropsType) => {
         {helmet.style.toComponent()}
         {helmet.script.toComponent()}
         {helmet.noscript.toComponent()}
-
-        <link rel="stylesheet" href="/css/vendor/font-awesome.min.css"/>
-        <link rel="stylesheet" href="/css/vendor/animate.min.css"/>
-        <link rel="stylesheet" href="/css/vendor/owl.carousel.css"/>
-        <link rel="stylesheet" href="/css/vendor/owl.transitions.css"/>
-        <link rel="stylesheet" href="/css/style.css"/>
-        <script src="/js/vendor/modernizr.js"></script>
       </head>
-      <body {...helmet.bodyAttributes.toComponent()} class="single" className="single">
+      <body {...helmet.bodyAttributes.toComponent()}>
         <Main />
         <State />
         <Script />
-        <script src="/js/vendor/jquery.js"></script>
-        <script src="/js/vendor/grid.js"></script>
-        <script src="/js/vendor/owl.carousel.min.js"></script>
-        <script src="/js/vendor/wow.min.js"></script>
-        <script src="/js/vendor/jquery.nav.js"></script>
-        <script src="/js/vendor/typed.min.js"></script>
-        <script src="/js/vendor/jquery.scrollUp.min.js"></script>
-        <script src="/js/vendor/scroll.js"></script>
-        <script src="/js/vendor/jquery.sticky.js"></script>
-        <script src="/js/vendor/jquery.flexnav.min.js"></script>
-        <script src="/js/vendor/masonry.pkgd.min.js"></script>
-        <script src="/js/vendor/skrollr.js"></script>
-        <script src="/js/script.js"></script>
       </body>
     </html>
   );
